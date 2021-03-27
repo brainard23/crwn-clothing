ae9e870b697903219a24de825a0543e6a16fe314
imakid14 <62824989+imakid14@users.noreply.github.com>
2021-03-27 20:22:42 +0800

    content component

---
+++ src/pages/homepage/homepage.compenent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Directoy from '../../components/directory/directory.component'
+
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+       <Directoy />
+    </div>
+);
+export default HomePage;
